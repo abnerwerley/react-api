35244ffa96d173adefe4004ecd541034ba321a92
abnerwerley <abnerwerley77@gmail.com>
2022-10-13 14:29:34 -0300

    @abnerwerley [Listing films' names and urls]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import './App.css';
+import { Component } from "react";
+import api from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    filmes: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get("star%20wars");
+
+    this.setState({ filmes: response.data });
+  }
+
+  render() {
+    const { filmes } = this.state;
+
+    return (
+      <div>
+        <h1>Listar os filmes</h1>
+        {filmes.map((filme) => (
+          <li key={filme.show.id}>
+            <h2>
+              {filme.show.name}
+            </h2>
+            <p>{filme.show.url}</p>
+          </li>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
